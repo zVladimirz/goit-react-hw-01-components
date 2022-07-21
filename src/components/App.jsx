7cc07ab5e3998beb83fce4817cbb0fd37f7a5b32
zVladimirz <tzxdlo@gmail.com>
2022-07-21 21:20:02 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,16 @@
-import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './FriendList/FriendList';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-
-import user from '../data/user.json';
-import data from '../data/data.json';
-import friends from '../data/friends.json';
-import transactions from '../data/transactions.json';
-
-const divStyle = {
-  width: '440px',
-  display: 'flex',
-  paddingTop: '20px',
-  paddingBottom: '20px',
-  justifyContent: 'center',
-  background: '#e7ecf2',
-};
-
 export const App = () => {
   return (
-    <>
-      <div style={divStyle}>
-        <Profile
-          username={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-        />
-      </div>
-      <div style={divStyle}>
-        <Statistics title="Upload stats" stats={data} />
-      </div>
-      <div style={divStyle}>
-        <Statistics stats={data} />
-      </div>
-      <div style={divStyle}>
-        <FriendList friends={friends} />
-      </div>
-      <div style={divStyle}>
-        <TransactionHistory items={transactions} />
-      </div>
-    </>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
 };
